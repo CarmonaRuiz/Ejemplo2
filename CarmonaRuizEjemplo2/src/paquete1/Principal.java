08bf35464bc05b2bed4bb8bd4068854ef2620657
DAM1 <DAM1@192.168.7.125>
2023-05-11 17:22:41 +0200

    comentario de crear una rama y validarla

--- CarmonaRuizEjemplo2/src/paquete1/Principal.java
+++ CarmonaRuizEjemplo2/src/paquete1/Principal.java
@@ -1,5 +1,17 @@
 package paquete1;
-
+/*A continuación se va a crear una rama en el repositorio local, fusionarla luego con el tronco y subirla al
+repositorio remoto.
+1) Para el elemento Branches se pulsa Botón derecho->Switch To-> New Branch. Llamarla como
+rama1 y marcar la casilla de verificación Check out new branch. ( Al marcar esta casilla nos
+moveremos directamente a esta nueva rama que se está creando).
+2) Crear dentro del proyecto un nuevo paquete (paqueteRama1) con una clase llamada Rama1, en
+cuyo método main.
+3) Confirmar este cambio en el repositorio local mediante la opción Commit.
+4) Seleccionar Botón derecho->Team->Show in History y visualizar todas las confirmaciones
+realizadas y cómo, en la rama1 aparece el indicador de HEAD.
+5) Subir esta rama al repositorio remoto, seleccionándola en el área de repositorios Git y pulsar Botón
+derecho->Push Branch->Preview->Finish.
+6) Mostrar esta nueva rama en GitHub.*/
 public class Principal {
 
 	public static void main(String[] args) {
